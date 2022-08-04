de5740fca5033901a2c427c2b41dd13667d1e807
minkee-kim <80187578+minkee-kim@users.noreply.github.com>
2022-08-04 18:31:03 +0900

    Time: 2 ms (93.63%), Space: 83.8 MB (49.60%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -3,14 +3,14 @@ class Solution {
     public int maxProfit(int[] prices) {
         // 작은 값과 return 해줄 max값이 필요
         // 루프를 돌리면서 계속 값을 비교하며 업데이트해야 함.
-        int min = prices[0];
-        int max = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         
         // min은 prices[i] 보다 작다면 그 값으로 변경해야 한다.
         // prices[i]-min과 max 중 더 큰 값을 찾는 이유는 결국 최대 이윤을 찾아야 하기 때문.
         // prices[i]-min 중 최대값이 최대 이윤이다. 
     
-        for(int i=1,j=prices.length; i<j; i++) {
+        for(int i=0,j=prices.length; i<j; i++) {
             if(prices[i] < min ) {
                 min = prices[i];
             } max = Math.max(max, prices[i]-min);
