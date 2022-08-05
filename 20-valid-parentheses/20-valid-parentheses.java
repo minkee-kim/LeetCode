1f02598ced1d01177c79e3ebc7f04a6336a8d88c
minkee-kim <80187578+minkee-kim@users.noreply.github.com>
2022-08-05 11:55:18 +0900

    Time: 2 ms (90.79%), Space: 40.1 MB (95.80%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+     Stack<Character> stack = new Stack<>();
+        char[] parenthesis = s.toCharArray();
+        int ed = parenthesis.length;
+        if(parenthesis[0]==')' || parenthesis[0]==']' || parenthesis[0]=='}'
+          || parenthesis[ed-1]=='(' || parenthesis[ed-1]=='{' || parenthesis[ed-1]=='['
+                || (ed%2) !=0
+        ) {
+            return false;
+        }
+
+        for(char pa : parenthesis) {
+            if (pa== '(' || pa=='{' || pa=='['){
+                stack.push(pa);
+            }
+                        if (stack.isEmpty()== true) 
+                return false;
+                            
+            if (pa==')' && stack.peek() =='(')  {
+                stack.pop();
+                continue;
+            } else if (pa==')' &&  stack.peek() !='(') {
+                return false;
+            }
+            if(pa=='}' && stack.peek() == '{') {
+                stack.pop();
+                continue;
+            } else if (pa=='}' && stack.peek()!='{') {
+                return false;
+            }
+            if (pa==']' && stack.peek() == '[') {
+                stack.pop();
+                continue;
+            } else if (pa == ']' && stack.peek()!= '[') {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+
+    }
+}
