85ae1037ab6120fcf5a2d89e21d85ae8cb7c6bd5
minkee-kim <80187578+minkee-kim@users.noreply.github.com>
2022-08-09 11:30:06 +0900

    Time: 0 ms (100.00%), Space: 42.9 MB (51.08%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,17 +9,23 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {    
-        ListNode p = null;
-        ListNode q = null;
-        ListNode r = head;
-        while (r != null) {
-            p = q;
-            q = r;
-            r = r.next;
-            q.next = p;
-        }
-        return q;
+    public ListNode reverseList(ListNode head) {
+    if(head==null) {
+                return head;
+            }
+            ListNode prev = null ;
+            ListNode present = head;
+            ListNode next = null;
+            while (present!=null) {
+                // 기존의 포인터 연결이 끊어지기 때문에 next에 현재 헤드의 다음 값을 저장
+                next =present.next;
+                // 포인터 역순
+                present.next = prev;
+                // prev에 현재 값을 대입
+                prev = present;
+                // present는 다음 노드로
+                present=next;
+            }
+            return prev;
     }
-        
-    }
+}
